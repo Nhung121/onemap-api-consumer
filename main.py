fae270444c7366d7717090b8a39ff01a8ae77914
Nhung Nguyen <nhn121@yahoo.com>
2019-05-29 21:38:06 +0800

    add rate limit

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+import time
 import requests
 import pandas as pd
 
 def get_coordinates_as_svy21(lat, lon):
-    url = "https://developers.onemap.sg/commonapi/convert/4326to3414"
-    response = requests.request("GET", url, params={"latitude":lat,"longitude":lon})
+    try:
+        url = "https://developers.onemap.sg/commonapi/convert/4326to3414"
+        response = requests.request("GET", url, params={"latitude":lat,"longitude":lon})
 
-    return response.json()
+        return response.json()    
+    except:
+        return (0, 0)
 
 # load data
 coordinates = pd.read_csv('./data/input_coordinates.csv')
+limit = 250
 
 # call onemap api for conversion
 for i, coordinate in coordinates.iterrows():
-    lat = coordinate['latitude']
-    lon = coordinate['longitude']
-
-    result = get_coordinates_as_svy21(lat=lat, lon=lon)
+    result = get_coordinates_as_svy21(lat=coordinate['latitude'], lon=coordinate['longitude'])
+    print(result)
 
     coordinates.loc[i, 'Y'] = result['Y']
     coordinates.loc[i, 'X'] = result['X']
 
-coordinates.to_csv('./data/output_coordinates.csv', index=False)
+    # rate limit
+    if i % limit == 0:
+        secs_to_sleep = 55
+        print(f'sleep for {secs_to_sleep} secs')
+        time.sleep(secs_to_sleep)
+
+        # save checkpoint 
+        _coordinates = coordinates.dropna()
+        _coordinates.to_csv(f'./data/output_coordinates-checkpoint-{i}.csv', index=False)
